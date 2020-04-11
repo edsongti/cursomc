4ff11d7dba1d586e1891260fd5ed6cbefbe58007
Edson Carlos <edsoncarlosgti@gmail.com>
2020-04-10 21:42:00 -0300

    Projeto criado

---
+++ src/main/java/com/edson/cursomc/CurscomcApplication.java
@@ -0,0 +1,13 @@
+package com.edson.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurscomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurscomcApplication.class, args);
+	}
+
+}
